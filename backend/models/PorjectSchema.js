26d772eb05721abe3c9d1be2654ac9885814338b
Dinesh <dineshgarig@gmail.com>
2025-04-29 11:11:32 +0530

    Schema complete

---
+++ backend/models/PorjectSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
+  });
+  
+const Project = mongoose.model('Project', projectSchema);
+module.exports =  Project;
---
+++ backend/models/TaskSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type:String,
+    required:true
+  },
+  description: {
+    type:String,
+    required:true
+  },
+  status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
+  createdAt: { type: Date, default: Date.now },
+  completedAt: Date
+});
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task
